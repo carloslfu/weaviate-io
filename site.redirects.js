844be7f052d1faba34d67315c7d701d79daaa4b2
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-12-20 14:13:48 +0100

    blog and roadmap redirects - take 2

--- site.redirects.js
+++ site.redirects.js
@@ -21,18 +21,18 @@ const siteRedirects = {
         if (existingPath.includes('/blog')) {
             // Redirect from /blog/2022/06/{X} to /blog/{X}
             return [
-                existingPath.replace('/blog', /\/blog\/202[12]\/[01][0-9]/),
-                // existingPath.replace('/blog', '/blog/2021/02'),
-                // existingPath.replace('/blog', '/blog/2021/03'),
-                // existingPath.replace('/blog', '/blog/2021/05'),
-                // existingPath.replace('/blog', '/blog/2021/11'),
-                // existingPath.replace('/blog', '/blog/2022/06'),
-                // existingPath.replace('/blog', '/blog/2022/07'),
-                // existingPath.replace('/blog', '/blog/2022/08'),
-                // existingPath.replace('/blog', '/blog/2022/09'),
-                // existingPath.replace('/blog', '/blog/2022/10'),
-                // existingPath.replace('/blog', '/blog/2022/11'),
-                // existingPath.replace('/blog', '/blog/2022/12'),
+                // existingPath.replace('/blog', /\/blog\/202[12]\/[01][0-9]/),
+                existingPath.replace('/blog', '/blog/2021/01'),
+                existingPath.replace('/blog', '/blog/2021/03'),
+                existingPath.replace('/blog', '/blog/2021/05'),
+                existingPath.replace('/blog', '/blog/2021/11'),
+                existingPath.replace('/blog', '/blog/2022/06'),
+                existingPath.replace('/blog', '/blog/2022/07'),
+                existingPath.replace('/blog', '/blog/2022/08'),
+                existingPath.replace('/blog', '/blog/2022/09'),
+                existingPath.replace('/blog', '/blog/2022/10'),
+                existingPath.replace('/blog', '/blog/2022/11'),
+                existingPath.replace('/blog', '/blog/2022/12'),
                 
                 //   existingPath.replace('/blog', '/docs/support'),
             ];

3922f78ca5320b6c827beadc5b03c76cf56cd631
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-12-20 17:31:34 +0100

    clean up

--- site.redirects.js
+++ site.redirects.js
@@ -1,8 +1,5 @@
 const siteRedirects = {
-    // fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
-    // toExtensions: ['exe', 'zip'], // /myAsset -> /myAsset.zip (if latter exists)
     redirects: [
-        // /docs/oldDoc -> /docs/newDoc
         {
             to: '/developers/weaviate/roadmap',
             from: [
@@ -99,7 +96,7 @@ const siteRedirects = {
     ],
     createRedirects(existingPath) {
         if (existingPath.includes('/blog')) {
-            // Redirect from /blog/2022/06/{X} to /blog/{X}
+            // Redirect from /blog/2022/06/{X} (etc) to /blog/{X}
             return [
                 existingPath.replace('/blog', '/blog/2021/01'),
                 existingPath.replace('/blog', '/blog/2021/02'),
@@ -161,116 +158,3 @@ const siteRedirects = {
 }
 
 module.exports = siteRedirects;
-
-// TODO: add these pages
-    // /resources.html
-    // /resources/gsoc.html
-    // /resources/gsod-summary.html
-    // /resources/gsod.html
-
-// /developers/weaviate/current/architecture
-// /developers/weaviate/current/architecture/binary-passage-retrieval.html
-// /developers/weaviate/current/architecture/cluster.html
-// /developers/weaviate/current/architecture/interface.html
-// /developers/weaviate/current/architecture/prefiltering.html
-// /developers/weaviate/current/architecture/resources.html
-// /developers/weaviate/current/architecture/storage.html
-// /developers/weaviate/current/benchmarks
-// /developers/weaviate/current/benchmarks/ann.html
-// /developers/weaviate/current/client-libraries
-// /developers/weaviate/current/client-libraries/cli.html
-// /developers/weaviate/current/client-libraries/go.html
-// /developers/weaviate/current/client-libraries/java.html
-// /developers/weaviate/current/client-libraries/javascript.html
-// /developers/weaviate/current/client-libraries/python.html
-// /developers/weaviate/current/configuration
-// /developers/weaviate/current/configuration/authentication.html
-// /developers/weaviate/current/configuration/authorization.html
-// /developers/weaviate/current/configuration/backups.html
-// /developers/weaviate/current/configuration/modules.html
-// /developers/weaviate/current/configuration/monitoring.html
-// /developers/weaviate/current/configuration/persistence.html
-// /developers/weaviate/current/configuration/vector-index-type.html
-// /developers/weaviate/current/core-knowledge
-// /developers/weaviate/current/core-knowledge/basics.html
-// /developers/weaviate/current/core-knowledge/clients.html
-// /developers/weaviate/current/core-knowledge/console.html
-// /developers/weaviate/current/core-knowledge/indexing.html
-// /developers/weaviate/current/getting-started
-// /developers/weaviate/current/getting-started/import.html
-// /developers/weaviate/current/getting-started/installation.html
-// /developers/weaviate/current/getting-started/modules.html
-// /developers/weaviate/current/getting-started/query.html
-// /developers/weaviate/current/getting-started/schema.html
-// /developers/weaviate/current/graphql-references
-// /developers/weaviate/current/graphql-references/additional-properties.html
-// /developers/weaviate/current/graphql-references/aggregate.html
-// /developers/weaviate/current/graphql-references/explore.html
-// /developers/weaviate/current/graphql-references/filters.html
-// /developers/weaviate/current/graphql-references/get.html
-// /developers/weaviate/current/graphql-references/vector-search-parameters.html
-// /developers/weaviate/current/installation
-// /developers/weaviate/current/installation/docker-compose.html
-// /developers/weaviate/current/installation/kubernetes.html
-// /developers/weaviate/current/installation/weaviate-cloud-service.html
-// /developers/weaviate/current/modules
-// /developers/weaviate/current/other-modules/custom-modules.html
-// /developers/weaviate/current/other-modules/spellcheck.html
-// /developers/weaviate/current/reader-generator-modules/ner-transformers.html
-// /developers/weaviate/current/reader-generator-modules/qna-openai.html
-// /developers/weaviate/current/reader-generator-modules/qna-transformers.html
-// /developers/weaviate/current/reader-generator-modules/sum-transformers.html
-// /developers/weaviate/current/retriever-vectorizer-modules/img2vec-neural.html
-// /developers/weaviate/current/retriever-vectorizer-modules/multi2vec-clip.html
-// /developers/weaviate/current/retriever-vectorizer-modules/ref2vec-centroid.html
-// /developers/weaviate/current/retriever-vectorizer-modules/text2vec-cohere.html
-// /developers/weaviate/current/retriever-vectorizer-modules/text2vec-contextionary.html
-// /developers/weaviate/current/retriever-vectorizer-modules/text2vec-huggingface.html
-// /developers/weaviate/current/retriever-vectorizer-modules/text2vec-openai.html
-// /developers/weaviate/current/retriever-vectorizer-modules/text2vec-transformers.html
-
-// /developers/weaviate/current/more-resources
-// /developers/weaviate/current/more-resources/deprecation-messages.html
-// /developers/weaviate/current/more-resources/example-datasets.html
-// /developers/weaviate/current/more-resources/example-use-cases.html
-// /developers/weaviate/current/more-resources/faq.html
-// /developers/weaviate/current/more-resources/glossary.html
-// /developers/weaviate/current/more-resources/migration-guide.html
-// /developers/weaviate/current/more-resources/performance.html
-
-// /developers/weaviate/current/restful-api-references
-// /developers/weaviate/current/restful-api-references/backups.html
-// /developers/weaviate/current/restful-api-references/batch.html
-// /developers/weaviate/current/restful-api-references/classification.html
-// /developers/weaviate/current/restful-api-references/meta.html
-// /developers/weaviate/current/restful-api-references/modules.html
-// /developers/weaviate/current/restful-api-references/nodes.html
-// /developers/weaviate/current/restful-api-references/objects.html
-// /developers/weaviate/current/restful-api-references/schema.html
-// /developers/weaviate/current/restful-api-references/well-known.html
-
-// /developers/weaviate/current/roadmap
-// /developers/weaviate/current/roadmap/architectural-roadmap.html
-// /developers/weaviate/current/roadmap/feature-roadmap.html
-
-// /developers/contributor-guide/current
-// /developers/contributor-guide/current/getting-started
-// /developers/contributor-guide/current/getting-started/improving-docs.html
-// /developers/contributor-guide/current/getting-started/commit-guidelines.html
-// /developers/contributor-guide/current/getting-started/git-and-github.html
-// /developers/contributor-guide/current/getting-started/reporting-bugs.html
-// /developers/contributor-guide/current/getting-started/writing-blogs.html
-// /developers/contributor-guide/current/getting-started/suggesting-enhancements.html
-// /developers/contributor-guide/current/weaviate-core
-// /developers/contributor-guide/current/weaviate-core/cicd.html
-// /developers/contributor-guide/current/weaviate-core/parsing-cross-refs.html
-// /developers/contributor-guide/current/weaviate-core/tests.html
-// /developers/contributor-guide/current/weaviate-core/setup.html
-// /developers/contributor-guide/current/weaviate-core/structure.html
-// /developers/contributor-guide/current/weaviate-module-system/architecture.html
-// /developers/contributor-guide/current/weaviate-module-system/how-to-build-a-new-module.html
-// /developers/contributor-guide/current/weaviate-module-system/overview.html
-// /developers/contributor-guide/current/weaviate-module-system
-// /developers/contributor-guide/current/weaviate-clients
-// /developers/contributor-guide/current/contextionary
-// /developers/contributor-guide/current/contextionary/classification-benchmarks.html
